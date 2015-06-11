dbdc916240f81273373d5fe3e8c013ec819435c4
Rodney <netmonger@users.noreply.github.com>
2015-06-11 17:12:53 -0500

    Added channel information parsing

--- find_channel.py
+++ find_channel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from lxml import etree
+import pprint
 
 tree = etree.parse('config.xml')
 
@@ -13,17 +14,30 @@ for elem in tree.iter('ip'):
 	if elem.text != ip:
 		continue
 	else:
+		# IP was found
 		found_ip=True
-		#print("Found IP")
-		#print((elem.iterancestors()).tag)
-		parent = elem.getparent()
-		port = parent.find('port').text
-		host_ip = parent.find('host-ip').text
-		feed = parent.find('feed').text
+
+		# Get parents element of section where IP was found
+		connection = elem.getparent()
+		channel = (connection.getparent()).getparent()
+
+		# Get text of all elements of a connection as well as channel information
+		connection_id = connection.attrib["id"]
+		port = connection.find('port').text
+		host_ip = connection.find('host-ip').text
+		feed = connection.find('feed').text
+		channel_id = channel.attrib["id"]
+		channel_label = channel.attrib["label"]
+
+		# Print all elements of a connection including channel information
+		print "\n"
+		print "Connection ID: %s" % connection_id
 		print "IP: %s" % ip
 		print "Port: %s" % port
 		print "Source IP: %s" % host_ip
 		print "Feed side: %s" % feed
+		print "Channel ID: %s" % channel_id
+		print "Channel label: %s" % channel_label
 		break
 
 # Inform user if IP was not found.
